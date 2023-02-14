7e6b9ffd6e7df63b371635b8f14f3271d7eaedd8
akhilsoman1234 <116093435+akhilsoman1234@users.noreply.github.com>
2023-02-14 10:17:00 +0530

    updates

--- resources/views/services/hire-blockchain-developers.blade.php
+++ resources/views/services/hire-blockchain-developers.blade.php
@@ -11,7 +11,7 @@
         <div class="shape bg-dot primary rellax w-16 h-20" data-rellax-speed="1" style="top: 3rem; left: 5.5rem"></div>
         <div class="overlap-grid overlap-grid-2">
           <div class="item">
-            <figure class="rounded shadow"><img src="https://sandbox.elemisthemes.com/assets/img/photos/about2.jpg" srcset="./assets/img/photos/about2@2x.jpg 2x" alt=""></figure>
+            <figure class="rounded shadow"><img src="{{asset('assets/img/services/blockchain-image.jpg')}}" srcset="{{asset('assets/img/services/blockchain-image.jpg')}} 2x" alt=""></figure>
           </div>
           <div class="item">
             <figure class="rounded shadow"><img src="{{asset('assets/img/services/blockchain.png')}}" srcset="{{asset('assets/img/services/blockchain.png')}}" alt=""></figure>
--- resources/views/services/hire-xamarin-developers.blade.php
+++ resources/views/services/hire-xamarin-developers.blade.php
@@ -10,7 +10,7 @@
         <div class="shape bg-dot primary rellax w-16 h-20" data-rellax-speed="1" style="top: 3rem; left: 5.5rem"></div>
         <div class="overlap-grid overlap-grid-2">
           <div class="item">
-            <figure class="rounded shadow"><img src="https://sandbox.elemisthemes.com/assets/img/photos/about2.jpg" srcset="./assets/img/photos/about2@2x.jpg 2x" alt=""></figure>
+            <figure class="rounded shadow"><img src="{{asset('assets/img/services/xamarin-image.jpg')}}" srcset="{{asset('assets/img/services/xamarin-image.jpg')}} 2x" alt=""></figure>
           </div>
           <div class="item">
             <figure class="rounded shadow"><img src="{{asset('assets/img/services/Xamarin.png')}}" srcset="{{asset('assets/img/services/Xamarin.png')}}" alt=""></figure>
