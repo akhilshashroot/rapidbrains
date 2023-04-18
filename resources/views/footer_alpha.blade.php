8b5149173e5418f9697571a19e890f5d61a9c7f3
lijimol-hashroot <92438222+lijimol-hashroot@users.noreply.github.com>
2023-04-18 14:51:52 +0530

    updated

---
+++ resources/views/footer_alpha.blade.php
@@ -0,0 +1,129 @@
+<div style="margin-top:5px"></div>
+
+<div style="margin-top:35px"></div>
+<footer class="bg-light">
+    <div class="container">
+        
+        
+        <div class="card image-wrapper bg-full bg-image bg-image mb-14" id="lazy" data-image-src="https://www.rapidbrains.com/assets/img/photos/RapidBrains-footerbg.webp" style="background-image: url(https://www.rapidbrains.com/assets/img/photos/RapidBrains-footerbg.webp);">        <div class="card-body p-10 p-xl-12">
+          <div class="row text-center">
+          <div class="col-xl-11 col-xxl-10 mx-auto">
+              <h2 class="fs-16 text-uppercase text-black mb-3">Hire Remote Developer To Empower Your Team</h2>
+              <h3 class="display-2 mb-8 px-lg-8 text-black">Build your remote team in just 24 hours and save your hiring time by 80%</h3>
+
+            </div>
+            <!-- /column -->
+          </div>
+          <!-- /.row -->
+          <div class="d-flex justify-content-center">
+            <span><a class="btn btn-white rounded" id="callbackreq">Hire Developers</a></span>
+          </div>
+        </div>
+        <!--/.card-body -->
+      </div>
+      <!--/.card -->
+        
+        
+        
+      
+      <div class="row gy-6 gy-lg-0">
+        <div class="col-md-4 col-lg-3">
+          <div class="widget">
+            <img class="mb-4" src="https://www.rapidbrains.com/assets/img/RapidBrains-logo.svg" srcset="https://www.rapidbrains.com/assets/img/RapidBrains-logo@2x.svg 2x" alt="RapidBrains Logo" width="200" style="margin-right:3rem;" />
+            <p class="mb-4">© 2023 RapidBrains. <br class="d-none d-lg-block" />A HashRoot Company</p>
+            <nav class="nav social ">
+               <a href="https://www.linkedin.com/company/rapidbrains" aria-label="RapidBrains linkedin account"><i class="uil uil-linkedin"></i></a>
+              <a href="https://www.facebook.com/rapidbrains" aria-label="RapidBrains facebook Account"><i class="uil uil-facebook-f"></i></a>
+              <a href="https://www.instagram.com/_rapidbrains/"  aria-label="RapidBrains instagram Account"><i class="uil uil-instagram"></i></a>
+             <a href="https://twitter.com/rapidbrains" aria-label="RapidBrains twitter Account"><i class="uil uil-twitter"></i></a>
+            </nav>
+            <!-- /.social -->
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-4 col-lg-3">
+          <div class="widget">
+            <h4 class="widget-title  mb-3">Get in Touch</h4>
+            <address class="pe-xl-15 pe-xxl-17"> Ground Floor, Athulya, Infopark, Kochi, Kerala, India</address>
+            
+            <a href="mailto:contact@rapidbrains.com" class="link-body">contact@rapidbrains.com</a><br /> +91 97784 26317
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-4 col-lg-3">
+          <div class="widget">
+            <h4 class="widget-title  mb-3">Learn More</h4>
+            <ul class="list-unstyled text-reset mb-0">
+              <li class="ft-pad"><a href="https://talents.rapidbrains.com/">Hire Talents</a></li>
+              <li class="ft-pad"><a href="https://jobs.rapidbrains.com/">Active Contracts</a></li>
+              <li class="ft-pad"><a href="https://www.rapidbrains.com/about">About Us</a></li>
+              <li class="ft-pad"><a href="https://www.rapidbrains.com/benefits">Our Benefits</a></li>
+              <li class="ft-pad"><a href="https://www.rapidbrains.com/process">The Process</a></li>
+                <li class="ft-pad"><a href="https://articles.rapidbrains.com">Articles</a></li>
+              <li><a href="https://www.rapidbrains.com/contact">Contact Us</a></li>
+            </ul>
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-12 col-lg-3">
+          <div class="widget">
+            <h4 class="widget-title  mb-3">Our Newsletter</h4>
+            <p class="mb-5">Subscribe to our newsletter to get our news & deals delivered to you.</p>
+            <div class="newsletter-wrapper">
+           <!-- Begin Mailchimp Signup Form -->
+              <div id="mc_embed_signup2">
+              <form  method="post" id="newsletter" novalidate="novalidate" action="#" enctype="multipart/form-data">
+                  <div id="mc_embed_signup_scroll2">
+                    <div class="mc-field-group input-group form-floating">
+                      <!-- <input type="email" value="" name="EMAIL" class="required email form-control" placeholder="Email Address" id="mce-EMAIL2"> -->
+                      <input type="text" class="form-control" id="emailnews" name="email" placeholder="Email Address">
+
+                      <label for="email">Email Address</label>
+                      <input type="submit" value="Join" id="subscribe" name="newsletter" class="btn btn-primary ">
+                      <p id="error-display" style="color:#dc3545;display:none">Please enter a valid email address</p>
+                    </div>
+                  </div>
+                </form>
+                <!--<form action="https://rapidbrains.us11.list-manage.com/subscribe/post?u=94f5ceb9f07b436db285d27b9&amp;id=cdc90d5b16&amp;f_id=009394e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll2">
+                    <div class="mc-field-group input-group form-floating">
+                      <input type="email" value="" name="EMAIL" class="required email form-control" placeholder="Email Address" id="mce-EMAIL2">
+                      <label for="mce-EMAIL2">Email Address</label>
+                      <input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe2" class="btn btn-primary ">
+                    </div>
+                    <div id="mce-responses2" class="clear">
+                      <div class="response" id="mce-error-response2" style="display:none"></div>
+                      <div class="response" id="mce-success-response2" style="display:none"></div>
+                    </div>
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ddc180777a163e0f9f66ee014_4b1bcfa0bc" tabindex="-1" value=""></div>
+                    <div class="clear"></div>
+                  </div>
+                </form>-->
+              </div>
+              <!--End mc_embed_signup-->
+            </div>
+            <!-- /.newsletter-wrapper -->
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+      </div>
+      <!--/.row -->
+      <p class="mb-2 mb-lg-0 mt-10 ">
+        <a href="https://www.rapidbrains.com/policies" class= "fs-sm">Privacy Policy |</a>
+        <a href="https://www.rapidbrains.com/non-disclosure-agreement" class= "fs-sm"> Non Disclosure Agreement |</a>
+        <a href="https://www.rapidbrains.com/acceptable-user-policy" class= "fs-sm"> Acceptable User Policy</a>
+        
+    </p>
+     <p >
+        <a href="https://www.rapidbrains.com/information-audit-document" class= "fs-sm">Information Audit Document |</a>
+        <a href="https://www.rapidbrains.com/data-processing-agreement" class= "fs-sm"> Data Processing Agreement</a>
+        
+    </p>
+      
+    </div>
+    <!-- /.container -->
+  </footer>
