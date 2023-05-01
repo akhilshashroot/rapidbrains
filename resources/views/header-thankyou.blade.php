59662366fd3443b5b41caa4a76759cef374507a1
akhilsoman1234 <116093435+akhilsoman1234@users.noreply.github.com>
2023-05-01 19:01:54 +0530

    ipdates

---
+++ resources/views/header-thankyou.blade.php
@@ -0,0 +1,143 @@
+<header class="position-absolute w-100">
+      
+      <nav class="navbar navbar-expand-lg center-nav transparent navbar-light">
+        <div class="container flex-lg-row flex-nowrap align-items-center">
+          <div class="navbar-brand w-100">
+          <a href="/">
+            <img src="{{asset('assets/img/RapidBrains-logo.svg')}}" srcset="{{asset('assets/img/RapidBrains-logo@2x.svg')}}" alt="RapidBrains Logo"  class="img-logo"  width="200" height="39"/>
+            </a>
+          </div>
+          <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
+            <div class="offcanvas-header d-lg-none">
+            <h3 class="text-white fs-30 mb-0">RapidBrains</h3>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown dropdown-mega">
+                  <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" style='color: #343f52;'>Hire Talents</a>
+                  <ul class="dropdown-menu mega-menu">
+                    <li class="mega-menu-content">
+                      <ul class="row row-cols-1 row-cols-lg-6 gx-0 gx-lg-6 gy-lg-1 list-unstyled">
+                      <li><a class="dropdown-item"  href="{{route('hire-react-js-developers')}}">React.js Developers </a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-python-developers')}}">Python Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-angular-developers')}}">Angular Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-node-js-developers')}}">Node.js Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-devops-developers')}}">DevOps Engineers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-mern-stack-developers')}}">MERN Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-php-developers')}}">PHP Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-android-developers')}}">Android Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-ionic-developers')}}">Ionic Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-react-native-developers')}}"> React Native Developers</a></li>                        
+                            <li><a class="dropdown-item" href="{{route('hire-shopify-developers')}}"> Shopify Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-remote-flutter-developers')}}"> Flutter Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-wordpress-developers')}}"> WordPress Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-kotlin-developers')}}">Kotlin Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-xamarin-developers')}}">Xamarin Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-mean-stack-developers')}}">MEAN Stack Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-ios-developers')}}">IOS Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-java-developers')}}">Java Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-magento-developers')}}">Magento Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-ui-ux-designers')}}">UX/UI Designers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-seo-specialists')}}">SEO Specialists</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-laravel-developers')}}">Laravel Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-aws-developers')}}">AWS Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-golang-developers')}}">Golang Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-django-developers')}}">Django Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-machine-learning-engineers')}}">ML Engineers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-asp-dot-net-developers')}}">ASP.NET Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-c-sharp-developers')}}">Csharp Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-react-node-developers')}}">React Node Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-back-end-developers')}}">Back End Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-front-end-developers')}}">Front End Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-mobile-developers')}}">Mobile Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-postgresql-developers')}}">Postgresql Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-full-stack-developers')}}">Full stack Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-javascript-developers')}}">Javascript Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-app-developers')}}">App Developers</a></li>
+                          
+                            <li><a class="dropdown-item" href="{{route('hire-blockchain-developers')}}">Blockchain Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-ruby-on-rails-developers')}}">Ruby on rails Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-drupal-developers')}}">Drupal Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-sap-developers')}}">SAP Developers</a></li>
+                            <li><a class="dropdown-item" href="{{route('hire-salesforce-developers')}}">Salesforce Developers</a></li>
+
+
+                      </ul>
+                      <!--/.row -->
+                    </li>
+                    <!--/.mega-menu-content-->
+                  </ul>
+                  <!--/.dropdown-menu -->
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" style='color: #343f52;'>Company</a>
+                    <ul class="dropdown-menu">
+                    <li class="nav-item"><a class="dropdown-item" href="{{route('about')}}"  >About RapidBrains</a></li>
+                      <li class="nav-item"><a class="dropdown-item"  href="{{route('benefits')}}">Our Benefits</a></li>
+                      <li class="nav-item"><a class="dropdown-item" href="{{route('process')}}">The Process</a></li>
+                     
+                    </ul>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="{{route('customsearch.index')}}" style='color: #343f52;'>Rate Card</a>
+                  </li>
+                 
+                  <li class="nav-item ">
+                    <a class="nav-link" href="https://talents.rapidbrains.com/" style='color: #343f52;'>Talent Cloud</a>
+                  </li>
+                </ul>
+                <!-- /.navbar-nav -->
+                <div class="offcanvas-footer d-lg-none">
+                  <div>
+                      
+                    <a href="mailto:contact@rapidbrains.com" class="link-inverse">contact@rapidbrains.com</a>
+                    <br /> +91 97784 26317 <br />
+                    
+                    
+                    <nav class="nav social social-white mt-4">
+                    <a href="https://www.linkedin.com/company/rapidbrains"><i class="uil uil-linkedin"></i></a>
+                    <a href="https://www.facebook.com/rapidbrains"><i class="uil uil-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/_rapidbrains/"><i class="uil uil-instagram"></i></a>
+                    <a href="https://twitter.com/rapidbrains"><i class="uil uil-twitter"></i></a>
+                    <!--  <a href="#"><i class="uil uil-youtube"></i></a>-->
+                    <!--</nav>-->
+                    <!-- /.social -->
+                  </div>
+                </div>
+                <!-- /.offcanvas-footer -->
+              </div>
+              <!-- /.offcanvas-body -->
+            </div>
+            <!-- /.navbar-collapse -->
+            <div class="navbar-other w-100 d-flex ms-auto">
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                
+                   
+                  <nav class="nav-item justify-content-end text-end">
+                     <span class="btn btn-primary btn-sm rounded-pill" id="enquirenowID"  style="color:#fff;">Enquire</span> 
+                    <!--<a href="#"><i class="uil uil-twitter"></i></a>-->
+                    <!--<a href="#"><i class="uil uil-facebook-f"></i></a>-->
+                    <!--<a href="#"><i class="uil uil-dribbble"></i></a>-->
+                    <!--<a href="#"><i class="uil uil-instagram"></i></a>-->
+                  </nav>
+                  <!-- /.social -->
+                
+                <li class="nav-item d-lg-none">
+                  <button class="hamburger offcanvas-nav-btn" aria-label="hamburger"><span></span></button>
+                </li>
+              </ul>
+              <!-- /.navbar-nav -->
+            </div>
+            <!-- /.navbar-other -->
+          </div>
+          <!-- /.navbar-collapse-wrapper -->
+        </div>
+        <!-- /.container -->
+      </nav>
+      <!-- /.navbar -->
+
+
+
+
+      </header>
