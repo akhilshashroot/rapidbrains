f5dd26c19eb4282ea46e9a5c89bf22802d0cea7a
marvanmuhammed <marvan.m@hashroot.com>
2023-10-19 16:34:18 +0530

    changes made

--- resources/views/services/hire-ai-developers.blade.php
+++ resources/views/services/hire-ai-developers.blade.php
@@ -344,7 +344,7 @@
     <!-- /.row -->
     <div class="row">
       <div class="col-lg-5 mx-auto text-center">
-      <a href="https://talents.rapidbrains.com/filter?skill=ai" class="btn btn-lg btn-primary btn-icon btn-icon-end">List all AI Developers with rates <i class="uil uil-arrow-up-right"></i></a>
+      <a href="https://talents.rapidbrains.com/filter?skill=ai" class="btn btn-lg btn-primary btn-icon btn-icon-end" style="color: #fff;">List all AI Developers with rates <i class="uil uil-arrow-up-right"></i></a>
       </div>
       <!-- /column -->
     </div>
--- resources/views/services/hire-cloud-engineers.blade.php
+++ resources/views/services/hire-cloud-engineers.blade.php
@@ -345,7 +345,7 @@
     <!-- /.row -->
     <div class="row">
       <div class="col-lg-5 mx-auto text-center">
-      <a href="https://talents.rapidbrains.com/filter?skill=Cloud Engineer" class="btn btn-lg btn-primary btn-icon btn-icon-end" style="color: #fff;">List all Hire Hire Cloud Engineers with rates <i class="uil uil-arrow-up-right"></i></a>
+      <a href="https://talents.rapidbrains.com/filter?skill=Cloud Engineer" class="btn btn-lg btn-primary btn-icon btn-icon-end" style="color: #fff;">List all Cloud Engineers with rates <i class="uil uil-arrow-up-right"></i></a>
       </div>
       <!-- /column -->
     </div>
